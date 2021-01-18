9c7a12a3c23b8b0b6271bc38c2f151b951bbf22b
Antonio <antoniohurtado92@gmail.com>
2021-01-18 23:17:29 +0100

    subjects

---
+++ routes/subject.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const subjectSchema = new Schema({
+
+    name: {type: String, required: [true, 'Nombre obligatorio.']},
+    description: String,
+    color: String,
+    creatorId: String,
+    date:{type: Date, default: Date.now}
+
+});
+
+// Convertir a un modelo
+const Subject = mongoose.model('Subject', subjectSchema);
+
+export default Subject;
