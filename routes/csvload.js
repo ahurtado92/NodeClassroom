30c0877bef1772e63ed872ff215b3d6e941a3e11
Antonio <antoniohurtado92@gmail.com>
2021-01-18 20:04:42 +0100

    csv-load

--- routes/csvload.js
+++ routes/csvload.js
@@ -9,19 +9,7 @@ router.post('/csv-load', verificarAuth, async(req, res) => {
     const body = req.body;
     body.usuarioId = req.usuario._id;
     
-    var query = [];
     try {
-      //const eventDB = await Event.create(body);
-      //res.status(200).json(eventDB); 
-      //await console.log(body)
-      body.forEach(element => {
-        console.log(element.classroom)
-        //const roomName = element.classroom
-        //console.log(rooms.name)
-        //query.id = element.id;
-        //query.roomId = room._id;
-        //console.log(query);
-      });
       await res.status(200).json(body);
     } catch (error) {
       return res.status(500).json({
